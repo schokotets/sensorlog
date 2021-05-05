0d635d729a265390a009fea5f85a2c349dc5e9f8
schokotets <schokotets@gmail.com>
2021-05-05 11:24:41 +0200

    initial commit
    a webservice that does two things:
    - write data that's passed via http to a SQLite DB
    - serve the DB's data for a certain date range via http
    
    also includes the website to showcase that data

---
+++ sensorlog.js
@@ -0,0 +1,105 @@
+const Koa = require('koa')
+const app = new Koa()
+const dayjs = require('dayjs')
+
+const bodyParser = require('koa-bodyparser')
+app.use(bodyParser({
+  enableTypes: ["text"]
+}))
+
+const serve = require('koa-static')
+app.use(serve('./static'))
+
+const sqlite3 = require('sqlite3')
+let connected = false
+var db = new sqlite3.Database('./sensordata.sql', err => {
+  if (err) {
+    console.log(err)
+  } else {
+    connected = true
+  }
+})
+db.run("CREATE TABLE IF NOT EXISTS sensordata (unixtime INTEGER, id INTEGER, temp REAL, relhum REAL, abshum REAL)")
+
+console.log("database tables created")
+
+let lines = []
+
+app.use(async ctx => {
+    if (ctx.url == "/log") {
+      console.log(ctx.request.body)
+      save(ctx.request.body)
+      console.log()
+      lines.push(ctx.request.body)
+    } else if (ctx.url.startsWith("/data")) {
+      let mode = ctx.request.query.mode
+
+      let nhoursQuery = ctx.request.query.nhours
+
+      let fromDateQuery = ctx.request.query.from
+      let toDateQuery = ctx.request.query.to
+
+      let fromDate
+      let toDate
+
+      if (nhoursQuery && mode != "dates") fromDate = dayjs().add(-parseInt(nhoursQuery),"hours")
+      else if (fromDateQuery) fromDate = dayjs(fromDateQuery)
+      else fromDate = dayjs().add(-36,"hours")
+
+      if(toDateQuery) toDate = dayjs(toDateQuery).endOf("day")
+      else toDate = dayjs()
+
+      console.log(toDate)
+      let dbdata = await getSensorData(fromDate.unix()*1000, toDate.unix()*1000)
+      console.log("done with db, starting JSON", (new Date().toISOString()))
+      ctx.body = JSON.stringify(dbdata)
+      console.log("done with JSON", (new Date().toISOString()))
+    } else {
+      ctx.body = 'Hello World\n' + lines.join("\n")
+    }
+})
+
+app.listen(8090)
+console.log("app listening on :8090")
+
+function save(str) {
+  let lines = str.split("\n")
+  data = []
+  for (l of lines) {
+    let [pin, th] = l.split(":")
+    let [temp, hum] = th.split(";")
+    pin = parseInt(pin)
+    temp = parseFloat(temp)
+    relhum = parseFloat(hum)
+    abshum = calAbsHum(temp, relhum)
+    data.push({pin, temp, relhum, abshum})
+  }
+  if (connected) {
+    for (s of data) {
+      let time = Date.now()
+      let querystring = `INSERT INTO sensordata(unixtime,id,temp,relhum,abshum) VALUES(${time},${s.pin},${s.temp},${s.relhum},${s.abshum})`
+      db.run(querystring)
+    }
+    console.log(`${new Date().toISOString()} inserted data`)
+  }
+}
+
+function calAbsHum(temp, hum) {
+  let abshum = 6.112 * Math.exp(17.67*temp/(temp+243.5)) * hum * 2.1674 / (273.15 + temp)
+  return Math.round(abshum * 10)/10
+}
+
+async function getSensorData(from, to) {
+  return new Promise((resolve, reject) => {
+    let rows = []
+    console.log("getting from db", (new Date().toISOString()))
+    let query = `SELECT * FROM sensordata WHERE unixtime >= ${from} AND unixtime <= ${to}`
+    db.each(query, (err, row) => {
+      if(err) reject(err)
+      else rows.push(Object.values(row))
+    }, (err, n) => {
+      if(err) reject(err)
+      else resolve(rows)
+    })
+  })
+}
---
+++ static/datepicker.js
@@ -0,0 +1,113 @@
+let nhoursinput = document.getElementById("nhours")
+let daterange = document.getElementById("daterange")
+let from = document.getElementById("from")
+let to = document.getElementById("to")
+
+function showcaseDates(startDate, endDate) {
+    if(!startDate) startDate = picker.getStartDate()
+    if(!endDate) endDate = picker.getEndDate()
+    daterange.value = `${formatDate(startDate, true)} bis ${formatDate(endDate, true)}`
+    from.value = `${formatDate(startDate, false)}`
+    to.value = `${formatDate(endDate, false)}`
+}
+
+var queryParams = {}
+
+function loadQueryParamsWithDefaults() {
+  if(Object.keys(queryParams).length > 0) return
+
+  let initNHours = 36
+  let qsNHours = /nhours=([0-9]*)/.exec(window.location.search)
+  if(qsNHours && "1" in qsNHours) {
+    initNHours = parseInt(qsNHours[1])
+  }
+  queryParams.nhours = initNHours
+
+  let initStartDate = new Date((new Date()).valueOf() - 2*1000*60*60*24)
+  let initEndDate = new Date()
+
+  let qsStartDate = /from=([0-9]*-[0-9]*-[0-9]*)/.exec(window.location.search)
+  let qsEndDate = /to=([0-9]*-[0-9]*-[0-9]*)/.exec(window.location.search)
+
+  if(qsStartDate && "1" in qsStartDate) {
+    initStartDate = new Date(qsStartDate[1])
+    queryParams.min = initStartDate
+    queryParams.mode = "dates"
+  }
+  queryParams.startDate = initStartDate
+  queryParams.fromDate = formatDate(initStartDate)
+
+  if(qsEndDate && "1" in qsEndDate) initEndDate = new Date(qsEndDate[1])
+  queryParams.endDate = initEndDate
+  queryParams.toDate = formatDate(initEndDate)
+
+  if (!("mode" in queryParams)) {
+    queryParams.min = dayjs().add(-initNHours,"hours")
+    queryParams.mode = "hours"
+    nhoursinput.value = initNHours
+  } else {
+    loadDatepicker()
+  }
+}
+
+function isHoursMode() {
+  loadQueryParamsWithDefaults()
+  return queryParams.mode == "hours"
+}
+
+var picker
+
+function loadDatepicker() {
+  if(picker) return
+  loadQueryParamsWithDefaults()
+
+  picker = new Litepicker({ 
+    element: document.getElementById('litepicker'),
+    singleMode: false,
+    startDate: queryParams.startDate,
+    endDate: queryParams.endDate,
+    maxDate: new Date(),
+    setup: picker => {
+
+      showcaseDates(queryParams.startDate, queryParams.endDate),
+      picker.on('selected', (startDate, endDate) => {
+        console.log('selected', startDate, endDate)
+        showcaseDates(startDate, endDate)
+      })
+    }
+  });
+
+  daterange.addEventListener('click', () => picker.show())
+}
+
+function loadNHours() {
+  loadQueryParamsWithDefaults()
+
+  if(nhoursinput.value) return
+  nhoursinput.value = queryParams.nhours
+}
+
+function twoDigit(n) {
+  if(n < 10) return '0'+n;
+  else return n;
+}
+
+function formatDate(date, hr) {
+  d = date
+  if ("dateInstance" in date) {
+    d = date.dateInstance
+  }
+  if (hr) {
+    return `${twoDigit(d.getDate())}.${twoDigit(d.getMonth()+1)}.${d.getFullYear()}`
+  } else {
+    return `${d.getFullYear()}-${twoDigit(d.getMonth()+1)}-${twoDigit(d.getDate())}`
+  }
+}
+
+function getStartDate(hr) {
+  return formatDate(picker.getStartDate(), hr)
+}
+
+function getEndDate(hr) {
+  return formatDate(picker.getEndDate(), hr)
+}
